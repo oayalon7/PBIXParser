866b63fe451d7170036c666cc7e9946edc0e3af6
Oren Ayalon <orenayalon7@gmail.com>
2021-02-11 15:52:52 -0500

    Page Name Suppot
    Added the ability to look at the pages each visual is associated with

--- PBIXParser.py
+++ PBIXParser.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 #This is the path for the layout file that is being read
-filename = "Report_Layout_finance"
+filename = "Report_Layout_justin"
 
 #this opens the file and cleans out any non ASCII text (<0x00>)
 with open(os.path.join(sys.path[0], filename), "r") as f:
@@ -23,7 +23,7 @@ data = []
 
 
 #function to parse through and identify visuals on the power bi report
-def visualparser(jsonVisual):
+def visualparser(pagename,jsonVisual):
 	
 	#get visual type
 	visualType = jsonVisual['config']["singleVisual"]["visualType"]
@@ -33,14 +33,14 @@ def visualparser(jsonVisual):
 
 	#get visual ID
 	visualID = jsonVisual['config']['name']
-	print(visualID)
+	#print(visualID)
 
 	#iterates through the visual dynamically pull all fields out ({y,x,category,value} field types in the visual)
 	def visualreader(x):
 		for key in x:
 			for y in x[key]:
 				#print("FIELD: " + y['queryRef'])
-				data.append([visualID,visualType,key,y['queryRef']])
+				data.append([pagename,visualID,visualType,key,y['queryRef']])
 
 
 	#executes the visualreader()
@@ -48,14 +48,14 @@ def visualparser(jsonVisual):
 
 
 #find all matching brackets to clean and identify visuals in the report
-def find_brackets(s):
+def find_brackets(s,open,close):
     toret = {}
     pstack = []
 
     for i, c in enumerate(s):
-        if c == '{':
+        if c == open:
             pstack.append(i)
-        elif c == '}':
+        elif c == close:
             if len(pstack) == 0:
                 raise IndexError("No matching closing brackets at: " + str(i))
             toret[pstack.pop()] = i
@@ -65,8 +65,11 @@ def find_brackets(s):
 
     return toret
 
+'''
+LEGACY CODE. NEW CODE ATTACHES THE PAGE NAME ASSOCIATED WITH THE VISUAL
+
 #where that magic happens. I put together all matching brackets identified and fill them in with the corresponding text. I fo
-list = find_brackets(basetext)
+list = find_brackets(basetext,'{','}')
 for key, value in list.items():
 	if basetext[key:value+1] != "{}":
 		configcheck = "\"config\":\"" + basetext[key:value+1]
@@ -74,17 +77,55 @@ for key, value in list.items():
 		if (basetext.find(configcheck) != -1):
 			visualjson = "{" + configcheck.replace("\\","") + "}"
 			cleanedjson = visualjson.replace("\"{","{").replace("}\"","}").replace("'\"","").replace("\"'","").replace("Top","\"Top\"")
-			print('----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+			#print('----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
 
 
 			if cleanedjson.find("themeCollection") == -1:
 				jsonvisual = json.loads(cleanedjson)
-				visualparser(jsonvisual)
+				#visualparser(jsonvisual)
 
-
+'''
 
 #resulting DataFrame
-VisualTable = pd.DataFrame(data, columns=['Visual ID','Visual Type', 'Field Category','Field Name'])
+#VisualTable = pd.DataFrame(data, columns=['Visual ID','Visual Type', 'Field Category','Field Name'])
+#VisualTable.to_csv(path_or_buf = "test.csv", index=False)
+
+
+#print(VisualTable)
+
+
+
+
+
+#find pages and vsuals within pages
+list = find_brackets(basetext,'[',']')
+for key, value in list.items():
+	if basetext[key:value+1] != "[]":
+		sectionscheck = "\"sections\":" + basetext[key:value+1]
+
+		if (basetext.find(sectionscheck) != -1):
+			visualjson = "{" + sectionscheck.replace("\\","") + "}"
+			cleanedjson = visualjson.replace("\"{","{").replace("}\"","}").replace("'\"","").replace("\"'","").replace("Top","\"Top\"")
+			#print(cleanedjson)
+			pagesjson = json.loads(cleanedjson)
+
+			sectionlist = pagesjson['sections']
+			
+			for page in sectionlist:
+				pagename = page['displayName']
+				#print(pagename)
+				#print(page)
+				#print(page['visualContainers'])
+
+				for visual in page['visualContainers']:
+					#print(visual['config'])
+					visualparser(pagename,visual)
+					#print('----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+
+				#print('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
+
+
+VisualTable = pd.DataFrame(data, columns=['Page Name','Visual ID','Visual Type', 'Field Category','Field Name'])
 VisualTable.to_csv(path_or_buf = "test.csv", index=False)
 
 
